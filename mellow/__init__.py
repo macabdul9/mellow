ac90db128bed8869b0445e305bf074ee216bed63
macabdul9 <abdulwaheed1513@gmail.com>
2019-06-24 01:32:57 +0530

    version 0.0.0.9

--- mellow/__init__.py
+++ mellow/__init__.py
@@ -1,4 +1,4 @@
 name = "mellow"
-__version__ = "0.0.0.7"
+__version__ = "0.0.0.9"
 author = "Abdul Waheed"
 motto = "Machine Learning - Just do it !"
--- mellow/utility.py
+++ mellow/utility.py
@@ -1,9 +1,11 @@
 import numpy as np
 
-def distance(p1, p2):
-    """ calculates the distance between two points in euclidean space
-    :param p1: numpy.array - euclidean coordinates
-    :param p2: numpy.array - euclidean coordinates
-    :return: numpy.float64
+
+def distance(self, p1, p2):
+    """
+    It is used to calculate the distance bewteen two points in Eucilidean Space
+    :param p1: coordinate of point one in Eucilidean Space
+    :param p2: coordinate of point two in Eucilidean Space
+    :return: distance between p1 and p2 float value
     """
     return np.sum((p2 - p1) ** 2) ** .5
